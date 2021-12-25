15be37d7494d792e19b2a9b271f3b9661b93fd63
Marcelo <marcelo.vedroni@cbrdoc.com.br>
2021-12-25 17:51:34 -0300

    Estrutura do formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
+import { useState } from "react"
+
 import { Dashboard } from "./components/Dashboard"
 import { Header } from "./components/Header"
+import { NewTransactionModal } from "./components/NewTransactionModal"
 import { GlobalStyle } from "./styles/global"
 
-import { useState } from "react"
-import Modal from 'react-modal'
-
-//lib pede p passar este objeto por questões de acessibilidade
-Modal.setAppElement('#root')
-
 export function App() {
-  const [isNewTransactionModal, setIsNewTransactionModal] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
   function handleOpenNewTransactionModal() {
-    setIsNewTransactionModal(true)
+    setIsOpen(true)
   }
 
-  function handleCloseNewTransactionModal() {
-    setIsNewTransactionModal(false)
+  function onRequestClose() {
+    setIsOpen(false)
   }
 
   return (
     <>    
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal
-        isOpen={isNewTransactionModal}
-        onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      <NewTransactionModal 
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+      />
       <GlobalStyle />
     </>     
   );
