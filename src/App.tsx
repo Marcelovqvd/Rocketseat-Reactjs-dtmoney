5acd64283f0d578b4e431cbc54354c349bc4c80c
Marcelo <marcelo.vedroni@cbrdoc.com.br>
2021-12-25 11:15:36 -0300

    Modal and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import { GlobalStyle } from "./styles/global"
-import { Header } from "./components/Header"
 import { Dashboard } from "./components/Dashboard"
+import { Header } from "./components/Header"
+import { GlobalStyle } from "./styles/global"
+
+import { useState } from "react"
+import Modal from 'react-modal'
 
 export function App() {
+  const [isNewTransactionModal, setIsNewTransactionModal] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModal(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModal(false)
+  }
+
   return (
-    <>
-      <Header />
+    <>    
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-     <GlobalStyle />
-    </>
-     
+      <Modal
+        isOpen={isNewTransactionModal}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+      <GlobalStyle />
+    </>     
   );
 }
